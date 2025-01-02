cc9b60a8068ccf8901cac19aaadc48210156d00a
Mohit Kr. Shaw <121444613+MohitShaw181104@users.noreply.github.com>
2025-01-02 20:17:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "./components/project1/hero";
 import View from "./components/project1/view";
-import Home from "./components/project2/home";
+// import Home from "./components/project2/home";
 
 const App = () => {
   return (
